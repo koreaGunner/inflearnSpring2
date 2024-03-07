695bc9ae194ccae9ef6822a06885b60ad745ecee
gunners <chakagae123@naver.com>
2024-03-07 15:47:50 +0900

    * 섹션 6. 중복 등록과 충돌

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
-@Component
+@Component //memoryMemberRepository
 public class MemoryMemberRepository implements MemberRepository{
 
     //ConcurrentHashMap 검색해보기
