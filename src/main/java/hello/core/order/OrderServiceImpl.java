2ccfad7bb4507994dff071a99551d94f65e03861
gunners <chakagae123@naver.com>
2024-03-08 17:40:16 +0900

    * 섹션 7. 다양한 의존관계 자동 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,17 +11,34 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
-
-    private final MemberRepository memberRepository;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    //필드주입 -> 문제가 많아서 쓰지 않음
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
+
+    //spring Bean인데 생성자가 한개일 때는 @Autowired 생략 가능
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+//    수정자 주입(setter 주입) -> 변경 가능성이 있으므로 변수에 final 지워야 한다
+//    @Autowired는 주입할 대상이 없으면 오류가 난다.
+//    주입할 대상이 없어도 동작하게 하려면 @Autowired(required = false) 하면 된다
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+//
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
