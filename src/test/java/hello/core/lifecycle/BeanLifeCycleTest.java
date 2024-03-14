87c6c914da29020410037b313106b387b5bd5b83
gunners <chakagae123@naver.com>
2024-03-14 17:22:01 +0900

    * 섹션 8. 인터페이스 InitializingBean, DisposableBean          빈 등록 초기화, 소멸 메서드 		 애노테이션 @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -20,7 +20,12 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean
+        //destroyMethod 에는 추론 기능이 내재돼있다.
+        //사용하기 싫으면 destroyMethod = "" 라고 하면된다
+        //추론기능은 close, shutdown 라는 이름의 메서드를 자동으로 호출해준다.
+        //@PostConstruct, @PreDestroy의 사용을 권장하나 코드를 고치지 못하는 외부 라이브러리에서는 사용하지 못함
+        //그럴경우 @Bean을 사용해야한다
+        @Bean/*(initMethod = "init", destroyMethod = "close")*/
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
