83a01973d099a840a3328ad9b0ea99bccceadd49
gunners <chakagae123@naver.com>
2024-03-14 16:09:46 +0900

    * 섹션 8. 빈 생명주기 콜백 시작

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 //현재 AppConfig에 @Configuration이 있으므로 ComponentScan의 대상이다
-//그러므로 Configuration은 ComponentScaan 대상에서 제외시켜야함(예제 코드를 지우기않기위해)
+//그러므로 Configuration은 ComponentScan 대상에서 제외시켜야함(예제 코드를 지우기않기위해)
 @ComponentScan(
         //basePackages = "hello.core.member",
         //basePackageClasses = AutoAppConfig.class,
